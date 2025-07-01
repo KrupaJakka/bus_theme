7676e64d169b56549d923feb67650353063e4705
KrupaJakka <krupaprincess159@gmaill.com>
2025-07-01 13:27:31 +0530

    loginscreens

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:bus_theme/apptheme.dart';
 import 'package:bus_theme/routes.dart';
 
-void main() {
-  runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => const BusTrackingApp(),
-    ),
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(DevicePreview(builder: (context) => BusTrackingApp()));
 }
 
 class BusTrackingApp extends StatelessWidget {
